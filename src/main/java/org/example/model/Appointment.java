29510b5d5793b66276347c736f72145f2306788e
mcmhmump <chuprikova.marina@list.ru>
2026-04-29 23:19:01 +0400

    Создала модели: хозяин, питомец, доктор и запись на прием

---
+++ src/main/java/org/example/model/Appointment.java
@@ -0,0 +1,54 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime appointmentDate;
+    private String diagnosis;
+    public Appointment(){}
+    public Appointment(LocalDateTime appointmentDate, String diagnosis){
+        this.appointmentDate=appointmentDate;
+        this.diagnosis=diagnosis;
+    }
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+    @Override
+    public String toString() {
+        return "Appointment{id=" + id + ", date='" + appointmentDate + "', diagnos="+diagnosis+ "}";
+    }
+}
---
+++ src/main/java/org/example/model/Owner.java
@@ -0,0 +1,57 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    public Owner(){}
+    public Owner(String name, String phoneNumber, String surname){
+        this.name=name;
+        this.surname=surname;
+        this.phoneNumber=phoneNumber;
+    }
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Pet> pets;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    @Override
+    public String toString() {
+        return "Owner{id=" + id + ", name='" + name + "', surname='" + surname + "'}";
+    }
+}
