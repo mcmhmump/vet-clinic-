00a81cf5f6396418414550cc191c79d34fe546a5
mcmhmump <chuprikova.marina@list.ru>
2026-06-04 19:21:32 +0400

    создала dto и добавила файлы request/response

---
+++ src/main/java/org/example/dto/DoctorResponse.java
@@ -0,0 +1,43 @@
+package org.example.dto;
+
+public class DoctorResponse {
+    private Long id;
+    private String name;
+    private String surname;
+    private String specialty;
+
+    public DoctorResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+}
---
+++ src/main/java/org/example/dto/OwnerRequest.java
@@ -0,0 +1,34 @@
+package org.example.dto;
+
+public class OwnerRequest {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+
+    public OwnerRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
